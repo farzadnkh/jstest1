2bef508fe53ba08df112053dadc4532a82a96a6d
farzad nikraftar <farzadnikraftar80@gmail.com>
2024-11-17 17:44:51 +0330

    i change the function

---
+++ script.js
@@ -0,0 +1,29 @@
+var safa = "85";
+
+function farzad() {
+    toString(4)
+console,log(69)
+return 'aliii'
+console.log(safa);
+ 
+if(false)
+
+
+
+
+switch (key) {
+    case value:
+        
+        break;
+
+    default:
+        break;
+}
+}
+
+
+
+
+
+
+
