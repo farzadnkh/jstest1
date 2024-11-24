eb01fabbf5eea36afde15853b6eb65acc316484d
farzad nikraftar <farzadnikraftar80@gmail.com>
2024-11-24 11:26:01 +0330

    i changed some issues

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 var safa = "85";
-var farzad = 5
+var farzad = 5;
 
 function hasan() {
-    toString(4)
-console.log(69)
-return 'aliii'
-/*
+  toString(4);
+  console.log(69);
+  return "aliii";
+  /*
 console.log(safa);
 'use strict'
 if (true) {
@@ -34,40 +34,26 @@ console.log(sum);
 }
 */
 
-var car1 = {
-    make : 'Ford',
-    model : 'Mustang',
-    year : 1969,
-}
+  var car1 = {
+    make: "Ford",
+    model: "Mustang",
+    year: 1969,
+  };
 
+  var car2 = new Object();
+  car2.make = "IranKhodro";
 
-var car2 = new Object();
-car2.make = 'IranKhodro';
+  // --------------------------------------
 
-// --------------------------------------
-
-function Car(make, model , year) {
+  function Car(make, model, year) {
     this.make = make;
     this.model = model;
     this.year = year;
-    this.run = function() {
-        return 'speed up to 200';
-    }
+    this.run = function () {
+      return "speed up to 200";
+    };
+  }
+
+  var car3 = new Car("ford", "mustang", 1969);
+  var car4 = new Car("Saipa", "111", 1800);
 }
-
-
-var car3 = new Car('ford' , 'mustang' , 1969);
-var car4 = new Car('Saipa' , '111' , 1800 );
-
-    
-
-
-j
-
-
-
-
-
-
-
-
